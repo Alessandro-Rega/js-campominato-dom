2dc03fb9431b21b75fdac59824333c2ea000b076
Alessandro-Rega <regaale47@gmail.com>
2021-11-18 14:37:14 +0100

    add script bombe

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
+const numeroBombe = 16;
+let mosse = 0;
+
 const play = document.getElementById("play");
 
 play.addEventListener('click', function(){
     const selezione = document.querySelector("#difficolta").value;
     const cont = document.querySelector(".container");
     cont.innerHTML = "";
+    const bombe = createBomb(numeroBombe, selezione);
+
+    console.log(bombe); //Vedere bombe
+
     for(let i = 1; i <= selezione; i++){
         const quadrato = addSquare(selezione, i);
         cont.appendChild(quadrato);
 
         quadrato.addEventListener('click', function(){
-            this.classList.add('clicked');
+            if(bombe.includes(parseInt(this.textContent)))bombaTrovata(this);
+            else this.classList.add('clicked');
+            mosse ++;
         });
     }
 
@@ -35,4 +44,19 @@ function addSquare(selezione, num) {
     if(selezione == 81)node.className = 'quadrato hard';
     else if(selezione == 49)node.className = 'quadrato crazy';
     return node;
+}
+
+function createBomb(numeroBombe, numeroCelle){
+    const array = [];
+    while(array.length < numeroBombe){
+        const numeroRandom = Math.floor(Math.random() * (numeroCelle - 1 + 1) ) + 1;
+        if(!array.includes(numeroRandom)) {
+            array.push(numeroRandom);
+        }
+    }
+    return array;
+}
+
+function bombaTrovata(elemento){
+    elemento.classList.add('bomb');
 }
